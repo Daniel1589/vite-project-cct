2918c8e006a30ef8c4d6b4d08dd044f68aaf2fb7
Daniel1589 <daniel1589@edu.aac.ac.il>
2024-11-05 18:34:33 +0200

    Finished NavBar decor.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,11 +4,14 @@ import { FaUserCircle } from "react-icons/fa";
 
 export const NavBar = () => {
   return (
-    <div className="bg-amber-50">
-      <nav className="flex flex-nowrap items-center whitespace-nowrap mt-6 mx-100 ">
+    <div className="mx-20">
+      <nav className="bg-amber-50 flex flex-nowrap items-center whitespace-nowrap mt-10 py-5">
         <div className="flex-shrink-0 px-8 list-none">
           <li>
-            <Link to="/" className="">
+            <Link
+              to="/"
+              className="flex items-center gap-1 no-underline text-black text-lg"
+            >
               <PiStarFourFill />
               The cleanic
             </Link>
@@ -16,29 +19,45 @@ export const NavBar = () => {
         </div>
         <div className="ml-auto flex gap-14 items-center list-none pr-8">
           <li>
-            <Link to="/plans" className="">
+            <Link
+              to="/plans"
+              className="no-underline text-black hover:underline"
+            >
               Plans
             </Link>
           </li>
           <li>
-            <Link to="/services" className="">
+            <Link
+              to="/services"
+              className="no-underline text-black hover:underline"
+            >
               Services
             </Link>
           </li>
           <li>
-            <Link to="/about" className="">
+            <Link
+              to="/about"
+              className="no-underline text-black hover:underline"
+            >
               About
             </Link>
           </li>
           <li>
-            <Link to="/login" className="flex items-center gap-2 ">
+            <Link
+              to="/login"
+              className="flex items-center gap-2 no-underline text-black"
+            >
               <p className="hover:text-gray-600">Log In</p>
-              <FaUserCircle />
+              <FaUserCircle className="pt-1 w-6 h-6" />
             </Link>
           </li>
           <li>
             <Link to="/booknow">
-              <input type="button" value="Book Now" className=""></input>
+              <input
+                type="button"
+                value="Book Now"
+                className="text-base bg-black text-white hover:text-black hover:bg-amber-50 rounded-full w-36 h-10 duration-[400ms] hover:cursor-pointer"
+              ></input>
             </Link>
           </li>
         </div>
